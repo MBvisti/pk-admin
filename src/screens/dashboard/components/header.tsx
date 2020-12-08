10632fcd80a83786cc1e5ce6db3dbc84357eb253
Morten Vistisen <mv@legalmonster.com>
2020-12-08 15:48:36 +0100

    linting

--- src/screens/dashboard/components/header.tsx
+++ src/screens/dashboard/components/header.tsx
@@ -5,7 +5,7 @@ interface HeaderProps {
     userName: string,
 }
 
-export const Header = ({userName}:HeaderProps) => {
+export const Header = ({userName}: HeaderProps) => {
     return (
         <div className="md:h-14 md:mt-6 md:flex md:justify-end md:items-center">
             <span className="md:mr-4 md:flex md:items-center">
