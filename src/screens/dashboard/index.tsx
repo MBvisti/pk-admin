f85fd4a392fe13c94cf1875619be17414805653b
Morten Vistisen <mv@legalmonster.com>
2020-12-08 11:09:08 +0100

    Update index.tsx

--- src/screens/dashboard/index.tsx
+++ src/screens/dashboard/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import {SideBar} from "./components/sideBar";
 
 function Admin() {
     return (
-        <div>this is admin</div>
+        <div className="border border-purple-600 md:flex md:min-h-screen md:min-w-full md:max-w">
+            <SideBar />
+        </div>
     )
 }
 
