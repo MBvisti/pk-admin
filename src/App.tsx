4df39fd2b4b321e6774b6fb7bb3dedfb8bb57882
Morten Vistisen <mv@legalmonster.com>
2020-12-08 16:48:37 +0100

    basic auth functionality setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-import React, {lazy, Suspense} from 'react';
-import {useAuth, UserData} from "./context/authContext";
+import React, {lazy, Suspense, useEffect, useState} from 'react';
+import {AuthPayload} from "./context/interfaces";
+import {useAuth} from "./context/authContext";
 import {LoadingScreen} from "./screens/loadingScreen";
 
 const Admin = lazy(() => import('./screens/dashboard'))
 const Login = lazy(() => import('./screens/login'))
 
-
 const App: React.FC = () => {
+    const [isAuth, setIsAuth] = useState(false)
     // TODO: when api docs are ready adding in authentication setup
-    const userData = useAuth() as UserData;
+    const data = useAuth() as AuthPayload;
+
+    useEffect(() => {
+        setIsAuth(data.userData.isAuthenticated)
+
+    }, [data.userData.isAuthenticated])
+
   return (
       <Suspense fallback={<LoadingScreen />}>
-          {userData.accessToken ? <Admin /> : <Login />}
+          {isAuth ? <Admin userName={data.userData.name} /> : <Login />}
       </Suspense>
   );
 }
