f75e3a64b11c43e95d4a675da4f29c898c1ae71e
Morten Vistisen <mv@legalmonster.com>
2020-12-08 12:23:22 +0100

    added sideBar comp and sideBarElement comp

--- src/screens/dashboard/components/sideBar.tsx
+++ src/screens/dashboard/components/sideBar.tsx
@@ -1,14 +1,123 @@
-import React from "react";
+import React, { useState } from "react";
 import {SideBarElement} from "./sideBarElement";
-import {AiOutlineHome} from "react-icons/all";
+import {
+    AiOutlineHome,
+    MdPlace,
+    BiEuro,
+    FaUsers,
+    RiParkingLine,
+    BsExclamationTriangle,
+    IoHardwareChipOutline, FiMessageSquare, FaWpforms, FiSettings
+} from "react-icons/all";
+
+interface SideBarState {
+    selectedElement: string | undefined;
+}
 
 export const SideBar = () => {
+    const [state, setState] = useState<SideBarState>({
+        selectedElement: "dashboard"
+    })
+
+    const handleActiveElement = (e: Event) => {
+        const element = e.currentTarget as HTMLDivElement;
+        const elName = element.dataset["element"];
+
+        setState({
+            ...state,
+            selectedElement: elName,
+        })
+    }
     return (
-        <div className="md:w-80 md:flex md:flex-col md:max-h-screen md:px-10 md:bg-gray-600">
-            <img className="md:mx-auto md:mt-6" src="https://parkeringskompagniet.dk/wp-content/uploads/2020/10/logologo-800x355-1-400x106.png" alt="parkeringskompagniet logo" />
-            <div className="md:mt-12">
-                <SideBarElement icon={<AiOutlineHome className="md:mr-4 md:h-6 md:w-6 md:stroke-current md:text-white" />}>Dashboard</SideBarElement>
-                <SideBarElement icon={<AiOutlineHome className="md:mr-4 md:h-6 md:w-6 md:stroke-current md:text-white" />}>Dashboard</SideBarElement>
+        <div className="md:w-80 md:flex md:flex-col md:max-h-screen md:bg-gray-600 md:overflow-y-hidden">
+            <img className="md:px-10 md:mt-6" src="https://parkeringskompagniet.dk/wp-content/uploads/2020/10/logologo-800x355-1-400x106.png" alt="parkeringskompagniet logo" />
+            <div className="md:mt-12 md:pl-10 md:relative md:overflow-y-scroll">
+                <SideBarElement
+                    handleActiveElement={(e: Event) => handleActiveElement(e)}
+                    icon={<AiOutlineHome className="md:mr-4 md:h-6 md:w-6 md:stroke-current md:text-white" />}
+                    isActive={state.selectedElement === "dashboard"}
+                    elementName={"dashboard"}
+                >
+                    Dashboard
+                </SideBarElement>
+                <SideBarElement
+                    handleActiveElement={(e: Event) => handleActiveElement(e)}
+                    icon={<MdPlace className="md:mr-4 md:h-6 md:w-6 md:stroke-current md:text-white" />}
+                    subLinks={["Parking statistics", "Parking list"]}
+                    isActive={state.selectedElement === "parking"}
+                    elementName={"parking"}
+                >
+                    Parking
+                </SideBarElement>
+                <SideBarElement
+                    handleActiveElement={(e: Event) => handleActiveElement(e)}
+                    icon={<BiEuro className="md:mr-4 md:h-6 md:w-6 md:stroke-current md:text-white" />}
+                    subLinks={["Charge package"]}
+                    isActive={state.selectedElement === "accounting"}
+                    elementName={"accounting"}
+                >
+                    Accounting
+                </SideBarElement>
+                <SideBarElement
+                    handleActiveElement={(e: Event) => handleActiveElement(e)}
+                    icon={<FaUsers className="md:mr-4 md:h-6 md:w-6 md:stroke-current md:text-white" />}
+                    subLinks={["User list", "User management"]}
+                    isActive={state.selectedElement === "users"}
+                    elementName={"users"}
+                >
+                    Users
+                </SideBarElement>
+                <SideBarElement
+                    handleActiveElement={(e: Event) => handleActiveElement(e)}
+                    icon={<RiParkingLine className="md:mr-4 md:h-6 md:w-6 md:stroke-current md:text-white" />}
+                    subLinks={["Resident parking", "Guest parking"]}
+                    isActive={state.selectedElement === "permits"}
+                    elementName={"permits"}
+                >
+                    Parking permits
+                </SideBarElement>
+                <SideBarElement
+                    handleActiveElement={(e: Event) => handleActiveElement(e)}
+                    icon={<BsExclamationTriangle className="md:mr-4 md:h-6 md:w-6 md:stroke-current md:text-white" />}
+                    subLinks={["Parking tickets", "Vehicle violation", "Property violation"]}
+                    isActive={state.selectedElement === "violations"}
+                    elementName={"violations"}
+                >
+                    Violations
+                </SideBarElement>
+                <SideBarElement
+                    handleActiveElement={(e: Event) => handleActiveElement(e)}
+                    icon={<IoHardwareChipOutline className="md:mr-4 md:h-6 md:w-6 md:stroke-current md:text-white" />}
+                    subLinks={["Live view"]}
+                    isActive={state.selectedElement === "hardware"}
+                    elementName={"hardware"}
+                >
+                    Hardware
+                </SideBarElement>
+                <SideBarElement
+                    handleActiveElement={(e: Event) => handleActiveElement(e)}
+                    icon={<FiMessageSquare className="md:mr-4 md:h-6 md:w-6 md:stroke-current md:text-white" />}
+                    isActive={state.selectedElement === "messages"}
+                    elementName={"messages"}
+                >
+                    Messages
+                </SideBarElement>
+                <SideBarElement
+                    handleActiveElement={(e: Event) => handleActiveElement(e)}
+                    icon={<FaWpforms className="md:mr-4 md:h-6 md:w-6 md:stroke-current md:text-white" />}
+                    isActive={state.selectedElement === "forms"}
+                    elementName={"forms"}
+                >
+                    Forms
+                </SideBarElement>
+                <SideBarElement
+                    handleActiveElement={(e: Event) => handleActiveElement(e)}
+                    icon={<FiSettings className="md:mr-4 md:h-6 md:w-6 md:stroke-current md:text-white" />}
+                    isActive={state.selectedElement === "settings"}
+                    elementName={"settings"}
+                >
+                    Settings
+                </SideBarElement>
             </div>
         </div>
     )
--- src/screens/dashboard/components/sideBarElement.tsx
+++ src/screens/dashboard/components/sideBarElement.tsx
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {IoIosArrowDown} from "react-icons/all";
 
 interface SideBarElementProps {
     icon: JSX.Element,
-    children: React.ReactChild
+    children: React.ReactChild,
+    subLinks?: Array<string>,
+    isActive: boolean,
+    handleActiveElement: any,
+    elementName: string,
 }
 
-export const SideBarElement = ({icon, children}: SideBarElementProps) => {
+export const SideBarElement = ({icon, children, subLinks, isActive, handleActiveElement, elementName}: SideBarElementProps) => {
+    const [isOpen, setIsOpen] = useState(isActive);
+
+    useEffect(() => {
+        setIsOpen(isActive)
+    }, [isActive])
     return (
-        <div className="border border-white text-white md:mb-4 md:flex md:items-center md:h-10 md:min-w-full">
-            {icon}
-            {children}
+        <div
+            data-element={elementName}
+            onClick={(e) => {
+            handleActiveElement(e)
+            setIsOpen(!isOpen)
+        }} className={`text-white cursor-pointer md:mb-2 md:flex md:flex-col md:min-w-full ${isActive ? "md:border-r-4 md:border-white" : "" }`}>
+            <div className="md:flex md:items-center md:h-10">
+                {icon}
+                {children}
+                {subLinks?.length !== undefined ? <IoIosArrowDown className="md:absolute md:right-6" /> : ""}
+            </div>
+            {
+                isOpen && subLinks !== undefined ?
+                <div className="md:ml-10 md:text-sm">
+                    {
+                        subLinks!.map((subLink, index) => (
+                            <p className="md:mb-2" key={index}>{subLink}</p>
+                        ))
+                    }
+                </div>
+                    : null
+            }
         </div>
     )
 }
