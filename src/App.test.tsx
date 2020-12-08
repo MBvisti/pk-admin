8b53d38ea9a8ae9af5600e5f747dabb3e81edc91
Morten Vistisen <mv@legalmonster.com>
2020-12-08 14:59:43 +0100

    useless test, but will be updated later on

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+// TODO: change this to be more useful - but not important for now
+test('renders app home page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Home/);
   expect(linkElement).toBeInTheDocument();
 });
