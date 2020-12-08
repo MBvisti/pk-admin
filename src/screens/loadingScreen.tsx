3cfd1c458028f8862460bcff75d0960add3c509d
Morten Vistisen <mv@legalmonster.com>
2020-12-08 15:48:00 +0100

    adding base loading screen

---
+++ src/screens/loadingScreen.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const LoadingScreen = () => {
+    return (
+        <div className="md:flex md:min-h-screen md:min-w-full md:max-w-full md:items-center md:justify-center">
+            <p>Loading...</p>
+        </div>
+    )
+}
