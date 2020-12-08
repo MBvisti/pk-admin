d3c9cc5a86e3b6e59075dbac30a59c7862d88364
Morten Vistisen <mv@legalmonster.com>
2020-12-08 17:18:07 +0100

    implementing loading msg when sending request

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,11 +1,22 @@
-import React, {ChangeEvent, FormEvent, useState} from 'react';
+import React, {ChangeEvent, FormEvent, Fragment, useState} from 'react';
 import { useAuth } from "../../context/authContext";
-import {AuthFunctions, UserAuthDetails} from '../../context/interfaces'
+import {AuthFunctions, AuthPayload} from '../../context/interfaces'
 
-function Login() {
-    const [state, setState] = useState<UserAuthDetails>({
+interface LoginState {
+    password: string,
+    username: string,
+    loading: boolean,
+}
+
+interface LoginProps {
+    isLoading: boolean,
+}
+
+function Login({isLoading}: LoginProps) {
+    const [state, setState] = useState<LoginState>({
         password: "",
         username: "",
+        loading: isLoading,
     });
     const authData = useAuth() as AuthFunctions;
 
@@ -22,11 +33,17 @@ function Login() {
         <div className="md:flex md:min-h-screen md:min-w-full md:max-w-full md:items-center md:justify-center md:bg-gray-200">
             <form
                 onSubmit={(e: FormEvent) => authData.login(e, state)}
-                className="bg-white md:rounded md:h-64 md:w-1/3 md:border md:border-black md:shadow-md md:py-6 md:px-10 md:flex md:flex-col md:items-center">
-                <h2 className="md:font-bold">Parkeringskompagniet administrator</h2>
-                <input onChange={(e: ChangeEvent) => handleFormInput(e)} value={state.username} className="md:w-full md:border md:border-black md:px-2 md:py-2 md:rounded md:mb-4 md:mt-4" name="username" type="text" placeholder="Please insert your username" required />
-                <input onChange={(e: ChangeEvent) => handleFormInput(e)} value={state.password} className="md:w-full md:border md:border-black md:px-2 md:py-2 md:rounded md:mb-4" name="password" type="password" placeholder="Please insert your username" required />
-                <button className="md:w-full md:h-10 md:text-white md:font-bolds md:rounded md:bg-blue-400 md:my-auto">Login</button>
+                className="bg-white md:rounded md:h-64 md:w-1/3 md:border md:border-black md:shadow-md md:py-6 md:px-10 md:flex md:flex-col md:items-center md:justify-center">
+                {
+                    isLoading ?
+                        <p>Loading...g</p> :
+                        <Fragment>
+                            <h2 className="md:font-bold">Parkeringskompagniet administrator</h2>
+                            <input onChange={(e: ChangeEvent) => handleFormInput(e)} value={state.username} className="md:w-full md:border md:border-black md:px-2 md:py-2 md:rounded md:mb-4 md:mt-4" name="username" type="text" placeholder="Please insert your username" required />
+                            <input onChange={(e: ChangeEvent) => handleFormInput(e)} value={state.password} className="md:w-full md:border md:border-black md:px-2 md:py-2 md:rounded md:mb-4" name="password" type="password" placeholder="Please insert your username" required />
+                            <button className="md:w-full md:h-10 md:text-white md:font-bolds md:rounded md:bg-blue-400 md:my-auto">Login</button>
+                        </Fragment>
+                }
             </form>
         </div>
     )
