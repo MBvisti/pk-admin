c1b9bee998534357f87917ac5e22f67cddf5fa70
Morten Vistisen <mv@legalmonster.com>
2020-12-17 13:38:46 +0100

    fixing border issues

--- src/screens/dashboard/components/sideBarElement.tsx
+++ src/screens/dashboard/components/sideBarElement.tsx
@@ -32,7 +32,9 @@ export const SideBarElement = ({
         setIsOpen(!isOpen);
       }}
       className={`text-white cursor-pointer md:mb-2 md:flex md:flex-col md:min-w-full ${
-        isActive ? "md:border-r-8 md:border-pk-blue" : ""
+        isActive
+          ? "md:border-r-8 md:border-t-0 md:border-l-0 md:border-b-0 md:border-pk-blue"
+          : ""
       }`}
     >
       <Link to={`/${elementName === "dashboard" ? "" : elementName}`}>
