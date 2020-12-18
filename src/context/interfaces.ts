00fbcc1361fbc2ca90ad502d65655a31e1450688
Morten Vistisen <mv@legalmonster.com>
2020-12-18 13:43:20 +0100

    updated authPayload to be called authData + formatting

--- src/context/interfaces.ts
+++ src/context/interfaces.ts
@@ -1,29 +1,28 @@
 export interface UserData {
-    name: string,
-    accessToken: string,
-    isAuthenticated: boolean,
+  name: string;
+  accessToken: string;
+  isAuthenticated: boolean;
 }
 
-
-export interface AuthPayload {
-    userData: UserData,
-    authFunctions: AuthFunctions,
-    loadingState: boolean,
+export interface AuthData {
+  userData: UserData;
+  error: string;
+  authFunctions: AuthFunctions;
+  loadingState: boolean;
 }
-
 
 export interface AuthFunctions {
-    login: (formEvent: React.FormEvent, state: UserAuthDetails) => {},
-    logout: any,
+  login: (e: React.FormEvent, payload: UserAuthDetails) => {};
+  logout: any;
 }
 
 export interface UserAuthDetails {
-    username: string,
-    password: string,
+  username: string;
+  password: string;
 }
 
 export interface AuthState {
-    userData: UserData,
-    loading: boolean,
-    error: string | null,
+  userData: UserData;
+  loading: boolean;
+  error: string | null;
 }
