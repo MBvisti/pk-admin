96edcb5e109dccf162d5b1b08243fcb6eb4bc1c6
Morten Vistisen <mv@legalmonster.com>
2020-12-18 11:39:59 +0100

    Update config.ts

--- src/http/config.ts
+++ src/http/config.ts
@@ -1,6 +1,6 @@
 export const axiosConfig = {
   // TODO: this is hard coded for now since there are some issues with netlify reading env variables
   // baseURL: process.env.NODE_ENV === "production" ? `${process.env.REACT_APP_BASE_URL_PROD}` : `${process.env.REACT_APP_BASE_URL_DEV}`,
-  baseURL: "http://135.181.45.75:8088/ParkingApp/rest",
+  baseURL: "https://test.apppark.dk/ParkingApp/rest",
   timeout: 4000,
 };
