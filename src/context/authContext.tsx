1e6db4e19945d8daeee2a45d9268a52b09c9f492
Morten Vistisen <mv@legalmonster.com>
2020-12-22 15:43:13 +0100

    Update authContext.tsx

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -8,7 +8,6 @@ import {
 } from "./interfaces";
 
 import { apiClient, endpoints, authentication } from "../http/api";
-import { AxiosError, AxiosResponse } from "axios";
 
 const AuthContext = React.createContext({});
 export const useAuth = () => React.useContext(AuthContext);
@@ -19,6 +18,7 @@ const AuthProvider = (props: any) => {
       name: "",
       accessToken: "",
       isAuthenticated: false,
+      userID: 0,
     },
     loading: false,
     error: null,
@@ -27,21 +27,31 @@ const AuthProvider = (props: any) => {
   // TODO: revisited this auth flow when getting closer to prod
   useEffect(() => {
     const tknDetails = Cookies.getJSON("pk-admin");
-    if (tknDetails) {
-      console.log(tknDetails["tknExpiry"] > Date.now());
+
+    if (!tknDetails) {
+      setState((s) => ({
+        ...s,
+        loading: false,
+      }));
+
+      return;
+    }
+
+    if (tknDetails["token"].length !== 0) {
       setState((s) => ({
         ...s,
         userData: {
           name: tknDetails.name,
           accessToken: tknDetails["token"],
           isAuthenticated: true,
+          userID: tknDetails.id,
         },
         loading: false,
       }));
 
       apiClient.interceptors.request.use(
         (config) => {
-          config.headers["Token"] = state.userData.accessToken;
+          config.headers["Token"] = tknDetails["token"];
           return config;
         },
         (error) => {
@@ -49,7 +59,43 @@ const AuthProvider = (props: any) => {
         }
       );
     }
-  }, [state.userData.accessToken]);
+
+    // apiClient.interceptors.response.use(
+    //   (res) => {
+    //     // everything is good - carry on
+
+    //     return res;
+    //   },
+    //   function (error) {
+    //     console.log("error from interceptors");
+    //     console.log(error);
+    //     if (error.response.status === 401) {
+    //       if (tknDetails) {
+    //         setState((s) => ({
+    //           ...s,
+    //           userData: {
+    //             name: tknDetails.name,
+    //             accessToken: tknDetails["token"],
+    //             isAuthenticated: true,
+    //             userID: tknDetails.id,
+    //           },
+    //           loading: false,
+    //         }));
+
+    //         apiClient.interceptors.request.use(
+    //           (config) => {
+    //             config.headers["Token"] = state.userData.accessToken;
+    //             return config;
+    //           },
+    //           (error) => {
+    //             console.log(error);
+    //           }
+    //         );
+    //       }
+    //     }
+    //   }
+    // );
+  }, []);
 
   // TODO: revisited this auth flow when getting closer to prod
   const login = async (e: React.FormEvent, payload: UserAuthDetails) => {
@@ -69,6 +115,7 @@ const AuthProvider = (props: any) => {
           name: res.data.user.name,
           accessToken: res.headers["token"],
           isAuthenticated: true,
+          userID: res.data.user.id,
         },
         loading: false,
         error: "no errors",
@@ -90,6 +137,7 @@ const AuthProvider = (props: any) => {
           token: res.headers["token"],
           name: res.data.user.name,
           tknExpiry: res.headers["tokenexpiry"],
+          userId: res.data.user.id,
         },
         {
           sameSite: "None",
@@ -116,6 +164,7 @@ const AuthProvider = (props: any) => {
         accessToken: "",
         name: "",
         isAuthenticated: false,
+        userID: 0,
       },
       loading: false,
       error: null,
